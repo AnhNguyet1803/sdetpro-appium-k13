6be70520275245c347153254e4295772733b77a3
nguyetpa <nguyetpham180397@gmail.com>
2024-01-30 11:37:09 +0700

    lab 4 update

--- src/labjava/lab4.java
+++ src/labjava/lab4.java
@@ -7,35 +7,50 @@ import java.util.Scanner;
 
 public class lab4 {
     public static void main(String[] args){
+        System.out.println("=====MENU======");
+        System.out.println("1. Print all numbers\n2. Print maximum value\n" +
+                "3. Print minimum value\n4. Search number");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please input a number: ");
+        int userNum = scanner.nextInt();
+        ArrayList<Integer> myListRandom = generateRandomArrayList(10);
 
         //Print all numbers
-        ArrayList<Integer> myListRandom = generateRandomArrayList(10);
-        System.out.println("Generated ArrayList: " + myListRandom);
-
-        //Print maximum value
-        int maxValue = findMaxValue(myListRandom);
-        System.out.println("Maximum value: " + maxValue);
-
-        //Print minimum value
-        int minValue = findMinValue(myListRandom);
-        System.out.println("Minimum value: " + minValue);
-
-        //Search number
-        Scanner scanner = new Scanner(System.in);
-        //Input number
-        System.out.print("Input number: ");
-        int yourNumber = scanner.nextInt();
-
-        int searchNumber = findNumber(myListRandom, yourNumber);
-
-        if (searchNumber != -1) {
-            System.out.println(yourNumber + " is found in the ArrayList at index: " + searchNumber);
-        } else {
-            System.out.println(yourNumber + " is not found in the ArrayList.");
+        if(userNum == 1){
+            System.out.println("Generated ArrayList: " + myListRandom);
         }
+        //Print maximum value
+        else if (userNum == 2) {
+            System.out.println("Generated ArrayList: " + myListRandom);
+            int maxValue = findMaxValue(myListRandom);
+            System.out.println("Maximum value: " + maxValue);
+        }
+        //Print minimum value
+        else if (userNum == 3) {
+            System.out.println("Generated ArrayList: " + myListRandom);
+            int minValue = findMinValue(myListRandom);
+            System.out.println("Minimum value: " + minValue);
+        }
+        //Search number
+        else if (userNum ==4) {
+            System.out.println("Generated ArrayList: " + myListRandom);
+            Scanner scanner1 = new Scanner(System.in);
+            //Input number
+            System.out.print("Input number: ");
+            int yourNumber = scanner1.nextInt();
 
-        scanner.close();
+            int searchNumber = findNumber(myListRandom, yourNumber);
 
+            if (searchNumber != -1) {
+                System.out.println(yourNumber + " is found in the ArrayList at index: " + searchNumber);
+            } else {
+                System.out.println(yourNumber + " is not found in the ArrayList.");
+            }
+            scanner.close();
+        } else {
+            System.out.println("Sorry!! Please input number in Menu!");
+        }
     }
 
     public static ArrayList<Integer> generateRandomArrayList(int size){
